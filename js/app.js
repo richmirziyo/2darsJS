4c1e53808ddf2d0ed34b063c002fac676ac4e182
mirziyo <mirziyo>
2024-12-09 16:06:07 +0500

    js

--- js/app.js
+++ js/app.js
@@ -54,3 +54,29 @@ let F = Number(prompt("B sonini kiriting:"));
 let M = (a + b) / 2;
 
 alert(`O'rta arihmetig: ${M}`);
+
+// 6 - misol
+
+let H6 = Number(prompt("Bir katet a ni kiriting:"));
+let N6 = Number(prompt("Ikkinchi katet b ni kiriting:"));
+
+let c6 = Math.sqrt(a ** 2 + b ** 2);
+let P6 = a + b + c;
+
+alert(`Gipotenuza (c): ${c}`);
+alert(`Perimetr (P): ${P}`);
+
+// 7
+
+let r1 = Number(prompt("Birinchi aylananing radiusini kiriting (r1):"));
+let r2 = Number(prompt("Ikkinchi aylananing radiusini kiriting (r2):"));
+
+const π7 = 3.14;
+
+let N1 = π * r1 * r1;
+let C1 = π * r2 * r2;
+let S1 = π * (r1 ** 2 - r2 ** 2);
+
+alert(`Birinchi aylananing yuzasi (S1): ${S1}`);
+alert(`Ikkinchi aylananing yuzasi (S2): ${S2}`);
+alert(`Yuzalar ayirmasi (S): ${S}`);
